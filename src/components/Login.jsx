1dd897c4cd7e6d34824b25b8dbae609fd84be2a1
divyanshuthakur14 <singhthakurdivyanshu@gmail.com>
2025-07-15 01:00:26 +0530

    newwerCommit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
 const handleLogin = async (e) => {
   e.preventDefault();
   try {
-    const res = await axios.post("https://tinycarebackend-ubpd.onrender.com/api/auth/login", {
+    const res = await axios.post("https://tinycarebackend-ubpd.onrender.com/api/users/login", {
       email,
       password,
     });
